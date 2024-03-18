f188c062352d3c80efb8c803890fe6a97a836281
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-18 13:48:04 -0400

    Update integer.py

--- integer.py
+++ integer.py
@@ -41,4 +41,19 @@ class Construct:
 # all possible neighbors are distance more than 1 unit away
 # for all possible distances, the set of neighbors with constant maximum distance forms shell
 # the constant unitary sequence of possible changes inside the environment, takes place at integer distance apart
-# each orbital pathway at constant distance define the relative position of the orbiting class
+# each orbital pathway at constant distance
+# define the relative position of the orbiting class
+
+language = object
+constructor = object
+
+# we could construct a new language with full interoperable constructors
+# apart from syntactic features there are universal approximators defined for every measurable attributes
+# key operators of any extrinsic property must imply some abstract variables
+# any intrinsic attribute of the abstract substrates (if) can be combined with any logical variables must imply some extrinsic attribute
+# if an intrinsic attribute implies another intrinsic attribute for both properties under union operation, there exists a substrate whose extrinsic properties imply a pair of possible intrinsic state
+# if an extrinsic state is an implication of operation performed on some physical variable and logical operator, there exists a physical quantity which never change under the same contructive transformation
+# if some physical quantity is kept invarient via combinations of logical and physical attributes some substrate, there exists a physical variable which must remain in existence for yielding other measurable quantities
+# if some measurable quantity that stays invarient undet the transformation applied on physical state of a substrate whose measure is implicated from the corresponding logical states, then the transformation implies some abstract state
+# when some abstract state of a system which is composed of subsystems whose intrinsic and extrinsic measures correspond to logical and physical interoperability criterion of each of their composite substructures, the super-structure uniting those substrates produce information variable
+# an information variable implies information media with reversible dynamics, therefore such dynamical laws encorporate the static properties on which abstract varible is imposed.
