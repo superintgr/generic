9090e3b4600222f4c89d8aca5c248d90f9a8ac0f
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-19 03:25:08 -0400

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,48 @@
+# there are two channels for input and four channels for output (2 x 4)
+# the device is devided into three subsystems: [sampler, transformer, observer]
+sampler = object
+transformer = object
+observer = object
+
+# for any change in the receptive fields of either channel, sampler will attend the nodes and measure the antinodes
+# measurement will collect 192000 samples per second of time from each sampler in their respective channel spaces
+measurer = object
+limiter = object
+constructor = object
+
+# transformer will transport the side information to the center of the field from each isolated substrates
+# substrate attributes of each channel are combined at the central limit and a set of information variable made available as possible observable attribute
+transporter = object
+quantizer = object
+destructor = object
+
+# the final subsystem is the observer which expects the computed parameters of the information variable under union of their combined product
+# possible observable attributes at the destructive phase of the transformation task are realized by the medium variable whose intersection with the substrate attributes of the observer yield all the relevant information
+translator = object
+computer = object
+recorder = object
+
+## the model of the system is constructed
+## all construction requires generic resource to be present in order to instantiate information processing substrates
+## the computation proceeds in the steps of finite duration and only a finite portion of the substrate attributes are used
+## it is asserted that information storage is unlimited via constructions made from processing substrates which are reversible computers
+## the checkpoints of existing models will be available while new layer of information must instantiate a new model checkpoint
+## both checkpoints are defined under the same identity where at any time the agent and system constitute a jointly isolated environment
+checkpoints = object
+agent = object
+system = object
+composite = object
+environment = object
+
+substrate = object
+layer = object
+classifier = object
+model = object
+
+# all the system models and agent subsystems are parametrized by a local buffer and named constructors accessible to the entire substrate during the lifetime of the physical objects associated with the transformations
+# locality principle is respected strictly by each model instances and fields in nature are used generally to transport any information that are not locally accessible physically to an environment
+# locally inaccessible information is a type of abstract constructor whose attributes defined under the computation medium is named in the buffer space
+buffer = object
+combine = object
+permute = object
+commute = object
