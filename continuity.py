dbf96057f1554b887bb9d1e34a0c95964008e21e
superintgr <155528797+superintgr@users.noreply.github.com>
2024-03-21 15:03:21 -0400

    Create continuity.py

---
+++ continuity.py
@@ -0,0 +1,64 @@
+"""
+- Bullet-points
+-- for every new node, there will be an introducing node
+-- if the pair of nodes are not same, continue to add the new node with a unique label and update the other node from within the node
+--- within a labeled node, the zero'th object requires two ones, connecting from each other to the zero position by equal and opposite distance.
+--- if a labeled node contains ones pair, the zero'th node would contain their possible extents.
+-- if a new node is labeled, its possible extents are contained in the introducing node.
+- Bullet-points
+"""
+
+01:10 (computation) : (preparation)
+10:01 (measurement) : (construction)
+01:10 (A) : (B)
+10:01 (C) : (D)
+
+(A) -> (D) : Interest -> Impulse
+(B) -> (C) : Code -> Potential
+
+Potential : whether the impulse vector will carry on the same interest forward or add a different potential in place that was previously seen
+
+Interest : often is around what has been previously interested with but couldn't get a closure from work done around the impulse
+
+Impulse : could be directly converted to a potential for recurring staticity or the interest started with will get successfully translated forward
+
+Code : any activity in coding environment consist of writing plain text, running previously seen code, getting help from bot, seeing if code runs, or finish part of an existing plan before moving forward
+
+01:10 :: Interest:Impulse
+Code:Potential :: 10:01
+
+01:10 :: Potential Interest:Potential Impulse
+Impulsive Programming:Programming Potential :: 10:01
+
+Programming Interest:Programming Impulse
+Impulsive Writing:Writing Potential
+
+.. Interest: There are specific interests around how code should be and what I would like to do about it
+.. Impulse: Usually starts with a pytorch model so that I could build a basis for my development
+
+.. Code: Writing that code is difficult because I don't like and have training samples for foundational type
+
+.. Potential: Usually would like to continue on my own plan which has to replace the usual method and I have to build it in my terms
+
+01:10 :: Computation:Preparation
+Measurement:Construction :: 10:01
+
+.. Computation: 
+
+input state @ fixed points.T
+transformed state.T @ target state, target state
+
+
+A @ x.T + b = y
+
+[t, tt, ttt] [fp] + [it] = [ti]
+[a, aa, aaa] [fp] + [ia] = [ai]
+[b, bb, bbb] [fp] + [ib] = [bi]
+
+A : matrix of output state
+x : constructor of the outohr state
+b : task for input -> output transform
+y : projected vector for input.T @ y -> output
+
+
+
